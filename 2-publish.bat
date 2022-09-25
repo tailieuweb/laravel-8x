4dacae005fbadfe4a5cd02d04aef91fa0fcf190a
tailieuweb <tailieuweb.com@gmail.com>
2022-09-26 04:08:49 +0700

    Update slideshow

---
+++ 2-publish.bat
@@ -0,0 +1,4 @@
+#php artisan vendor:publish --provider="Foostart\Category\CategoryServiceProvider" --force
+#php artisan vendor:publish --provider="Foostart\Acl\Authentication\AuthenticationServiceProvider" --force
+php artisan vendor:publish --provider="Foostart\Filemanager\FilemanagerServiceProvider" --force
+
